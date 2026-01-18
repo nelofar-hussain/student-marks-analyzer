60f2159369bf4a6fb32271a7b92fa0ea56d93ed1
NELOFAR HUSSAIN <nelofarh01@gmail.com>
2026-01-18 18:52:53 +0500

    Update marks.py

--- marks.py
+++ marks.py
@@ -1,1 +1,32 @@
-print("Hello,this is my first GitHub project")
+# Student Marks Analyzer
+
+name = input("Enter student name: ")
+
+total_marks = 500
+obtained_marks = int(input("Enter obtained marks out of 500: "))
+
+percentage = (obtained_marks / total_marks) * 100
+
+if percentage >= 80:
+    grade = "A"
+elif percentage >= 70:
+    grade = "B"
+elif percentage >= 60:
+    grade = "C"
+elif percentage >= 50:
+    grade = "D"
+else:
+    grade = "F"
+
+result = (
+    "Student Name: " + name + "\n"
+    "Obtained Marks: " + str(obtained_marks) + "\n"
+    "Percentage: " + str(percentage) + "%\n"
+    "Grade: " + grade
+)
+
+file = open("output.txt", "w")
+file.write(result)
+file.close()
+
+print("Result saved successfully!")
