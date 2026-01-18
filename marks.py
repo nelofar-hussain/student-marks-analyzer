6017bc3b62b9b57c5a279c548686f186577179c6
NELOFAR HUSSAIN <nelofarh01@gmail.com>
2026-01-18 18:55:56 +0500

    Update marks.py

--- marks.py
+++ marks.py
@@ -1,32 +1,49 @@
-# Student Marks Analyzer
+# Student Marks Analyzer using Functions and Loops
 
-name = input("Enter student name: ")
+def calculate_grade(percentage):
+    if percentage >= 80:
+        return "A"
+    elif percentage >= 70:
+        return "B"
+    elif percentage >= 60:
+        return "C"
+    elif percentage >= 50:
+        return "D"
+    else:
+        return "F"
 
-total_marks = 500
-obtained_marks = int(input("Enter obtained marks out of 500: "))
 
-percentage = (obtained_marks / total_marks) * 100
+def analyze_student():
+    name = input("Enter student name: ")
+    obtained_marks = int(input("Enter obtained marks out of 500: "))
 
-if percentage >= 80:
-    grade = "A"
-elif percentage >= 70:
-    grade = "B"
-elif percentage >= 60:
-    grade = "C"
-elif percentage >= 50:
-    grade = "D"
-else:
-    grade = "F"
+    total_marks = 500
+    percentage = (obtained_marks / total_marks) * 100
+    grade = calculate_grade(percentage)
 
-result = (
-    "Student Name: " + name + "\n"
-    "Obtained Marks: " + str(obtained_marks) + "\n"
-    "Percentage: " + str(percentage) + "%\n"
-    "Grade: " + grade
-)
+    result = (
+        "Student Name: " + name + "\n"
+        "Obtained Marks: " + str(obtained_marks) + "\n"
+        "Percentage: " + str(round(percentage, 2)) + "%\n"
+        "Grade: " + grade + "\n"
+        "-------------------------\n"
+    )
 
-file = open("output.txt", "w")
-file.write(result)
-file.close()
+    file = open("output.txt", "a")
+    file.write(result)
+    file.close()
 
-print("Result saved successfully!")
+    print("Result saved!\n")
+
+
+def main():
+    while True:
+        analyze_student()
+        choice = input("Do you want to add another student? (yes/no): ")
+        if choice.lower() != "yes":
+            break
+
+    print("All student results saved in output.txt")
+
+
+main()
